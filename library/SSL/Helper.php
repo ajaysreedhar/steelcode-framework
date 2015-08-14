f9c2da1487051be37c0e99fd74a4c1cac3526c13
Ajay Sreedhar <steelcode468@gmail.com>
2015-08-15 00:09:50 +0530

    copyright year changed to 2015

---
+++ library/SSL/Helper.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Helper.php - SSL helper functions
+ *
+ * Copyright (C) 2015 Ajay Sreedhar <ajaysreedhar468@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/**
+ * Class Steelcode_SSL_Helper
+ *
+ * Steelcode SSL helper functions
+ *
+ * @category Steelcode
+ * @package Steelcode_SSL
+ */
+abstract class Steelcode_SSL_Helper {
+
+	/**
+	 * Check whether SSL is used
+	 *
+	 * @return bool
+	 */
+	public static function isSSL() {
+		if ( isset( $_SERVER['HTTPS'] ) && 'on' == strtolower( $_SERVER['HTTPS'] ) )
+			return true;
+
+		return false;
+	}
+
+	/**
+	 * Check whether the URL is https:// or not
+	 * @return bool
+	 */
+	public static function isHttps() {
+		return self::isSSL();
+	}
+}

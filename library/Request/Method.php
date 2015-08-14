289f0150a5bafd39155a4658e0754d032471d1c0
Ajay Sreedhar <steelcode468@gmail.com>
2015-08-15 00:08:40 +0530

    copyright year changed to 2015

---
+++ library/Request/Method.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Method.php - Steelcode request method
+ *
+ * Copyright (C) 2015 Ajay Sreedhar <ajaysreedhar468@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/**
+ * Class Steelcode_Request_Method
+ *
+ * @category SteelCode
+ * @package Steelcode_Request
+ */
+class Steelcode_Request_Method {
+
+	/**
+	 * Flag to set whether prefix is to be used or not
+	 *
+	 * @var bool
+	 */
+	private $_usePrefix = false;
+
+	/**
+	 * Request parameter prefix
+	 *
+	 * @var string
+	 */
+	private $_prefix = "";
+
+	/**
+	 * Create index with prefix
+	 *
+	 * @param string $index
+	 * @return string
+	 */
+	private function _index( $index ) {
+		return ( ( $this->_usePrefix === true ) ? "{$this->_prefix}_{$index}" : "{$index}" );
+	}
+
+	/**
+	 * Get $_GET value
+	 *
+	 * @param string $parameter
+	 * @return mixed
+	 */
+	public function get( $parameter ) {
+		$index = $this->_index( $parameter );
+		return ( isset( $_GET[$index] ) ? $_GET[$index] : null );
+	}
+
+	/**
+	 * Get $_POST value
+	 *
+	 * @param string $parameter
+	 * @return mixed
+	 */
+	public function post( $parameter ) {
+		$index = $this->_index( $parameter );
+		return ( isset( $_POST[$index] ) ? $_POST[$index] : null );
+	}
+}

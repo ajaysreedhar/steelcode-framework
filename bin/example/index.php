95fbb121f15830fe2acd0ca1ec2ff767d94ae9cc
Ajay Sreedhar <steelcode468@gmail.com>
2015-08-15 13:01:27 +0530

    application path passed as a parameter

--- bin/example/index.php
+++ bin/example/index.php
@@ -1,19 +1,19 @@
 <?php
 /* Define this constant to make sure that all other files
  * are NOT directly accessed and are invoked only from index.php */
-define ( "LOAD_INC", true );
+define ( 'LOAD_INC', true );
 
-defined( "ABSPATH" ) || define( "ABSPATH", dirname( __FILE__ ) . "/" );
+defined( 'ABSPATH' ) || define( 'ABSPATH', dirname( __FILE__ ) . "/" );
 
-defined( "APPLICATION_PATH" ) || define( "APPLICATION_PATH", ABSPATH . "application/" );
+defined( 'APPLICATION_PATH' ) || define( 'APPLICATION_PATH', ABSPATH . 'application/' );
 
 /* Define path to the core steelcode classes
  * You may change this path if the core files are installed in some other directory */
-define( "INCLUDE_PATH", "Steelcode/" );
+define( 'INCLUDE_PATH', 'Steelcode/' );
 
-require_once ( INCLUDE_PATH . "steelcode.php" );
+require_once ( INCLUDE_PATH . 'steelcode.php' );
 
-$application = new Steelcode_Application();
+$application = new Steelcode_Application( APPLICATION_PATH );
 $controller  = $application->getControllerPath();
 
-$application->runApplication( APPLICATION_PATH );
+$application->run();

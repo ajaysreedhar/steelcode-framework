5ee6b1c8cde48713ae68f4e24f0e5330d159a551
Ajay Sreedhar <steelcode468@gmail.com>
2015-08-22 11:46:06 +0530

    method readCookie added

--- library/Http/Request/Headers.php
+++ library/Http/Request/Headers.php
@@ -1,4 +1,11 @@
 <?php
 class Steelcode_Http_Request_Headers {
-	
+
+	public function readCookie( $cookie ) {
+		if ( isset( $_COOKIE[$cookie] ) ) {
+			return $_COOKIE[$cookie];
+		}
+
+		return null;
+	}
 }
